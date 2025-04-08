1b5701b90cd111f1bf966f7dfb533d48bacd69b1
Davitmach <wvime30@gmail.com>
2025-04-08 17:23:41 +0400

    dea

--- app/components/full/full.tsx
+++ app/components/full/full.tsx
@@ -4,15 +4,11 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export const Full = () => {
-    const path = usePathname();
-    const {push} = useRouter()
+
     useEffect(() => {
-        const Data = localStorage.getItem('data');
-        if(path.includes('user') && Data) { 
-            push('/');
-        }
+     
         setTimeout(() => {
-            console.log(1);
+          
             
             
             
