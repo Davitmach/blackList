3a35858aeb1f70e5057f4afd7898bb526ee39af0
Davitmach <wvime30@gmail.com>
2025-04-09 20:38:43 +0400

    de

--- app/components/tg/tg.tsx
+++ app/components/tg/tg.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import { useTelegramButtons } from '@/app/hooks/btn';
-
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 export default function TelegramControlsClient() {
+  const {push} = useRouter();
+  const path = usePathname();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const navigationEntry = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming
+      if (navigationEntry?.type === 'reload' && path !== '/') {
+        push('/')
+      }
+    }
+  }, [])
   useTelegramButtons();
   return null; // ничего не рендерим, просто хук
 }
